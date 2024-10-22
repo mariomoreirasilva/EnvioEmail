ee4677a2a82d38bb227b6e0d7ad83a1d3f871df0
mariomoreirasilva <mario@desban.org.br>
2024-10-22 18:30:59 -0300

    Projeto criado, EmailDTO teste

---
+++ src/main/java/com/devsuperior/integrations/resources/EmailResource.java
@@ -0,0 +1,22 @@
+package com.devsuperior.integrations.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.integrations.dto.EmailDTO;
+
+@RestController
+@RequestMapping(value = "/emails")
+public class EmailResource {
+	
+	@PostMapping
+	public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+
+}
